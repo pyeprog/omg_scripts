1255623d7c1723fb528bbdd992365ea712b296fb
pyeprog <pyeprog@foxmail.com>
2019-06-02 21:42:44 +0800

    add system script

---
+++ resource/.vimrc
@@ -0,0 +1,85 @@
+set nocompatible
+filetype off
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+Plugin 'VundleVim/Vundle.vim'
+
+Plugin 'scrooloose/nerdtree'
+Plugin 'pyeprog/oneTerminal'
+Plugin 'easymotion/vim-easymotion'
+Plugin 'ctrlpvim/ctrlp.vim'
+Plugin 'jiangmiao/auto-pairs'
+Plugin 'tpope/vim-surround'
+Plugin 'tpope/vim-repeat'
+Plugin 'tpope/vim-commentary'
+Plugin 'unblevable/quick-scope'
+Plugin 'davidhalter/jedi-vim'
+
+Plugin 'google/vim-maktaba'
+Plugin 'google/vim-codefmt'
+Plugin 'google/vim-glaive'"
+call vundle#end()            
+call glaive#Install()
+
+filetype plugin indent on  
+
+syntax on
+set nu
+set smartindent
+set cindent
+set autoindent
+set nobackup
+set shiftwidth=4
+set tabstop=4
+set softtabstop=4
+set smarttab
+set expandtab
+set nowrap
+set hidden
+
+colorscheme desert
+
+let mapleader=" "
+
+" Quick Html tag
+imap << </
+
+" Add new line above or below
+nnoremap <silent><C-j> :set paste<CR>m`o<Esc>``:set nopaste<CR>
+nnoremap <silent><C-k> :set paste<CR>m`O<Esc>``:set nopaste<CR>
+
+" NERDTree
+map <C-n> :NERDTreeToggle<CR>
+
+" CtrlP
+let g:ctrlp_map='<c-p>'
+let g:ctrlp_cmd='CtrlP'
+
+" EasyMotion
+let g:EasyMotion_do_mapping=0
+let g:EasyMotion_smartcase=1
+" Turn on case insensitive feature
+let g:EasyMotion_smartcase=1
+" JK motions: Line motions
+let g:EasyMotion_startofline=1
+nmap <Leader><Leader>s <Plug>(easymotion-s)
+
+" Black
+nmap <Leader>== :FormatCode<CR>
+
+" Jedi
+let g:jedi#goto_command = "<C-]>"
+let g:jedi#documentation_command = "K"
+let g:jedi#usages_command = "<Leader>su"
+let g:jedi#completions_command = "<C-Space>"
+let g:jedi#rename_command = "<Leader>rn"
+let g:jedi#auto_vim_configuration = 0
+set completeopt=menuone,longest
+
+" Custom
+nnoremap <left> :bp<cr>
+nnoremap <right> :bn<cr>
+nnoremap <up> :tabp<cr>
+nnoremap <down> :tabn<cr>
--- system/init-system.sh
+++ system/init-system.sh
@@ -3,8 +3,6 @@
 set -x  # debug
 set -e
 
-su - root
-
 # Distro
 distro=$(hostnamectl | sed 's/^ *//' | grep 'Operating System' | cut -d ' ' -f3)
 if [ distro -ne 'CentOS' ]; then
@@ -12,13 +10,84 @@ if [ distro -ne 'CentOS' ]; then
     exit 1
 fi
 
+function showline() {
+    echo "==========$@=========="
+}
+
 # User
 function initUser() {
-    adduser pd
-    echo 'pd      ALL=(ALL)       ALL' >> /etc/sudoers
+    name=pd
+    if [[ $# > 0 ]]; then
+        name=$1
+    fi
+    adduser $name
+    if [[ -z $(grep $name /etc/sudoers) ]]; then
+        echo "$name      ALL=(ALL)       ALL" >> /etc/sudoers
+        sudo passwd $name
+        echo "add $name to /etc/sudoers"
+    fi
+}
+
+# System
+function initSystem() {
+    # sshd
+    sudo sed -ie 's/#ClientAliveInterval.*$/ClientAliveInterval 3/' /etc/ssh/sshd_config
+    sudo sed -ie 's/#ClientAliveCountMax.*$/ClientAliveCountMax 3600/' /etc/ssh/sshd_config
+    sudo service sshd restart
+}
+
+# vim
+function initVim() {
+    pip3 install --upgrade pip
+    sudo yum install vim -y
+    pip3 install autopep8 yapf jedi --user
+
+    if [[ -f $HOME/.vimrc ]]; then
+        rm -f $HOME/.vimrc
+    fi
+    cp ../resource/.vimrc $HOME/.vimrc
+    if [[ ! -d $HOME/.vim/bundle/Vundle.vim ]]; then
+        git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+    fi
+    vim +PluginInstall +qa
 }
 
 # Tools
 function initTools() {
-    
+    showline init tools
+
+    sudo yum install git -y
+    sudo yum install python36 python36-pip -y
+    sudo yum install golang -y
+    sudo yum install docker -y
+    sudo systemctl start docker
+    initVim && echo "init vim done"
+
+    showline init done
 }
+
+# Main
+case $1 in
+    user)
+        if [[ $# > 1 ]]; then
+            initUser $2
+        else
+            echo "specify user name"
+        fi
+        break
+        ;;
+    system)
+        initSystem
+        break
+        ;;
+    tool)
+        initTools
+        break
+        ;;
+    vim)
+        initVim
+        break
+        ;;
+    *)
+        echo "available: user system tool vim"
+esac
