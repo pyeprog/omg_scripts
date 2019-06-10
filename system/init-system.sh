d3eed86a03348489208140266091e092387fa343
pyeprog <pyeprog@foxmail.com>
2019-06-10 12:21:02 +0800

    add initUser to initJenkins

--- system/init-system.sh
+++ system/init-system.sh
@@ -3,6 +3,8 @@
 # set -x  # debug
 set -e
 
+default_password='412351860'
+
 # Distro
 distro=$(hostnamectl | sed 's/^ *//' | grep 'Operating System' | cut -d ' ' -f3)
 if [[ distro -ne 'CentOS' ]]; then
@@ -20,12 +22,16 @@ function initUser() {
     if [[ $# > 0 ]]; then
         name=$1
     fi
-    adduser $name
-    if [[ -z $(grep $name /etc/sudoers) ]]; then
-        echo "$name      ALL=(ALL)       ALL" >> /etc/sudoers
-        sudo passwd $name
-        echo "add $name to /etc/sudoers"
+    id "$name" > /dev/null 2>&1
+    if [[ $? > 0 ]]; then
+        adduser $name
     fi
+    if [[ ! -d /etc/sudoers.d ]]; then
+        sudo mkdir -p /etc/sudoers.d
+    fi
+    echo "$name      ALL=(ALL)       ALL" | sudo tee `/etc/sudoers.d/$name`
+    echo -e "$default_password\n$default_password" | sudo passwd $name
+    echo "add $name to /etc/sudoers"
 }
 
 # System
@@ -34,7 +40,7 @@ function initSystem() {
     sudo sed -ie 's/#ClientAliveInterval.*$/ClientAliveInterval 3/' /etc/ssh/sshd_config
     sudo sed -ie 's/#ClientAliveCountMax.*$/ClientAliveCountMax 3600/' /etc/ssh/sshd_config
     if [[ -z $(sudo grep 'DenyUsers root' /etc/ssh/sshd_config) ]]; then
-        sudo echo 'DenyUsers root' | sudo tee -a /etc/ssh/sshd_config
+        echo 'DenyUsers root' | sudo tee -a /etc/ssh/sshd_config
 	sudo sed -ie 's/PermitRootLogin.*$/PermitRootLogin no/' /etc/ssh/sshd_config
     fi
     sudo sed -ie 's/#Port 22/Port 4123/' /etc/ssh/sshd_config
@@ -64,6 +70,19 @@ function initVim() {
     vim +PluginInstall +qa
 }
 
+# add user jenkins to docker group
+function jenkinsDockerConfig() {
+    jenkins_user='jenkins'
+    docker_group=`cat /etc/group | cut -d ':' -f1 | grep docker`
+    sudo systemctl status docker > /dev/null
+    docker_exist=$?
+    sudo systemctl status jenkins > /dev/null
+    jenkins_exist=$?
+    if [[ $docker_exist == 0  && $jenkins_exist == 0 ]]; then
+        sudo usermod -aG $jenkins_user $docker_group
+    fi
+}
+
 # jenkins
 function initJenkins() {
     sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
@@ -72,6 +91,15 @@ function initJenkins() {
     sudo yum install jenkins -y
     sudo systemctl start jenkins
     sudo systemctl enable jenkins
+    jenkinsDockerConfig
+    initUser jenkins
+}
+
+# docker
+function initDocker() {
+    sudo yum install docker -y
+    sudo systemctl start docker
+    jenkinsDockerConfig
 }
 
 # nginx
@@ -91,11 +119,10 @@ function initTools() {
     sudo yum install git -y
     sudo yum install python36 python36-pip -y
     sudo yum install golang -y
-    sudo yum install docker -y
-    sudo systemctl start docker
+    initDocker && echo 'init docker done'
     initVim && echo 'init vim done'
-    initJenkins && echo 'init jenkins'
-    initNginx && echo 'init nginx'
+    initJenkins && echo 'init jenkins done'
+    initNginx && echo 'init nginx done'
 
     showline init done
 }
@@ -124,6 +151,9 @@ case $1 in
     nginx)
         initNginx
         ;;
+    docker)
+        initDocker
+        ;;
     *)
-        echo "available: user system tool vim jenkins"
+        echo "available: user system tool vim jenkins docker nginx"
 esac
